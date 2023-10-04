a1e36bc9305b127f0b91ea9a746ba3147aa3ac6f
Ruslan <rusikeabudana@gmail.com>
2023-10-04 20:53:36 +0600

    Убрал функцию, сделал через генератор списка!

--- Module17/08_caesar_cipher/main.py
+++ Module17/08_caesar_cipher/main.py
@@ -1,17 +1,22 @@
 # TODO здесь писать код
-def char_shift(symbol, shift):
-    alphabet = [chr(i) for i in range(ord('а'), ord('я') + 1)]
-    for i in range(len(alphabet)):
-        if symbol == alphabet[i]:
-            symbol = alphabet[(i + shift) % len(alphabet)]
-            break
-    return symbol
+
+# alphabet = [chr(i) for i in range(ord('а'), ord('я') + 1)]
+#
+#
+# def char_shift(symbol, _shift):
+#     for i in range(len(alphabet)):
+#         if symbol == alphabet[i]:
+#             symbol = alphabet[(i + _shift) % len(alphabet)]
+#             break
+#     return symbol
+
 
 message = input('Введите сообщение: ')
 shift = int(input('Сдвиг: '))
 
 
-new_message = (''.join([char_shift(symbol, shift) for symbol in message]))
+# new_message = (''.join([char_shift(symbol, shift) for symbol in message]))
+alphabet = [chr(i) for i in range(ord('а'), ord('я') + 1)]
+new_message = [alphabet[(alphabet.index(char) + shift) % len(alphabet)] if char in alphabet else char for char in message]
+new_message = ''.join(new_message)
 print(new_message)
-
-
