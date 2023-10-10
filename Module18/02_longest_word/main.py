72160431983354f85061d306d68497db03847782
Ruslan <rusikeabudana@gmail.com>
2023-10-10 16:22:56 +0600

    Первое выполнение 9 заданий 18 модуля

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+string = input('Введите строку: ').split()
+max_str = ''
+
+for word in string:
+    if len(word) > len(max_str):
+        max_str = word
+
+print('Самое длинное слово: {}'.format(max_str))
+print('Длина слова: {}'.format(len(max_str)))
--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+flag = True
+
+while True:
+    password = input('Введите пароль: ')
+
+    len_of_digit = len([char for char in password if char.isdigit()])
+    len_of_upper = len([char for char in password if char.isupper()])
+    len_password = len(password)
+
+    if len_of_digit < 3 or len_password < 8 or len_of_upper < 1:
+        print('Пароль ненадёжный. Попробуйте ещё раз.')
+    else:
+        print('Это надёжный пароль!')
+        break
+
