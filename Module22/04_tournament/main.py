170a0f8474565ed80b231c92d732dd262dc97c26
Ruslan <rusikeabudana@gmail.com>
2023-12-09 20:25:45 +0600

    Первое выполнение модуля 22

--- Module22/04_tournament/main.py
+++ Module22/04_tournament/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+file_out = open('first_tour.txt', 'r', encoding='utf-8')
+file_in = open('second_tour.txt', 'w', encoding='utf-8')
+file_in.write('\n')
+coef = file_out.readline().strip()
+number_of_second = 0
+for i_line in file_out:
+    lst = i_line.split()
+    # print(lst)
+    if int(lst[2]) > int(coef):
+        number_of_second += 1
+        file_in.write(str(number_of_second) + ')' + ' '.join(lst) + '\n')
+file_in.seek(0)
+file_in.write(str(number_of_second))
+file_out.close()
+file_in.close()
